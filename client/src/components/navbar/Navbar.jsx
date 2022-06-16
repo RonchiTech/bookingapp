edd9f937eceedade44a33fa05268c408f3b8d9c3
ronchitech <ronchibusiness@gmail.com>
2022-06-16 11:30:29 +0800

    theme colors updated

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div className="navbar">
       <div className="navContainer">
         <Link to="/">
-          <span className="logo">SeaBCD</span>
+          <span className="logo">seaBcD</span>
         </Link>
         <div className="navItems">
           <button className="navButton">Register</button>
